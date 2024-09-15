8d6d52123b408b21e3dc4de9c220e0fda7cbcad3
Saksham Verma <sakshamverma1000@gmail.com>
2024-09-15 22:49:55 +0530

    Created the Play effect over video and the animation on h1 in #page1

---
+++ script.js
@@ -0,0 +1,39 @@
+let videoContainer = document.getElementById("video-container");
+let playElement = document.getElementById("play");
+console.dir(playElement);
+
+videoContainer.addEventListener("mouseenter", () => {
+  gsap.to(playElement, {
+    scale: 1,
+    opacity: 1,
+  });
+});
+
+videoContainer.addEventListener("mouseleave", () => {
+  gsap.to(playElement, {
+    scale: 0,
+    opacity: 0,
+  });
+});
+
+videoContainer.addEventListener("mousemove", (details) => {
+  gsap.to(playElement, {
+    left: details.x - 50,
+    top: details.y - 80,
+  });
+});
+
+gsap.from("#page1 h1", {
+  y: 100,
+  opacity: 0,
+  delay: 0.3,
+  stagger: 0.3,
+  duration: 0.9,
+});
+
+gsap.from("#video-container", {
+  opacity: 0,
+  delay: 1.3,
+  duration: 1.5,
+  y: 100,
+});
