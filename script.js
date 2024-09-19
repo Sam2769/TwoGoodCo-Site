f2fb2e600d1e9bcf918094aa9c112abf43a4a47d
Saksham Verma <sakshamverma1000@gmail.com>
2024-09-19 22:36:56 +0530

    While trying to add ScrollTrigger, Locomotive.js was disabled

--- script.js
+++ script.js
@@ -1,32 +1,72 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
+const newFunction = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+};
+
+newFunction;
 
 let videoContainer = document.getElementById("video-container");
 let playElement = document.getElementById("play");
-console.dir(playElement);
 
-videoContainer.addEventListener("mouseenter", () => {
-  gsap.to(playElement, {
-    scale: 1,
-    opacity: 1,
+let videoFunction = () => {
+  videoContainer.addEventListener("mouseenter", () => {
+    gsap.to(playElement, {
+      scale: 1,
+      opacity: 1,
+    });
   });
-});
 
-videoContainer.addEventListener("mouseleave", () => {
-  gsap.to(playElement, {
-    scale: 0,
-    opacity: 0,
+  videoContainer.addEventListener("mouseleave", () => {
+    gsap.to(playElement, {
+      scale: 0,
+      opacity: 0,
+    });
   });
-});
 
-videoContainer.addEventListener("mousemove", (details) => {
-  gsap.to(playElement, {
-    left: details.x,
-    top: details.y - 70,
+  videoContainer.addEventListener("mousemove", (details) => {
+    gsap.to(playElement, {
+      left: details.x,
+      top: details.y - 70,
+    });
   });
-});
+};
+
+videoFunction;
 
 gsap.from("#page1 h1", {
   y: 100,
@@ -44,6 +84,7 @@ gsap.from("#video-container", {
 });
 
 let cursor = document.querySelector("#cursor");
+console.log(cursor);
 
 document.addEventListener("mousemove", (dets) => {
   gsap.to(cursor, {
@@ -53,5 +94,20 @@ document.addEventListener("mousemove", (dets) => {
 });
 
 let childArray = Array.from(document.getElementsByClassName("child"));
-console.log(childArray);
-console.log(childArray[0]);
+
+let displayCursor = () => {
+  gsap.to(cursor, {
+    transform: "translate(-50%, -50%) scale(1)",
+  });
+};
+
+let hideCursor = () => {
+  gsap.to(cursor, {
+    transform: "translate(-50%, -50%) scale(0)",
+  });
+};
+
+childArray.forEach((elem) => {
+  elem.addEventListener("mouseenter", displayCursor);
+  elem.addEventListener("mouseleave", hideCursor);
+});
